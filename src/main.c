7d1aa84eec0aef1912ce0ea0b6fa91d1c71d708f
łukasz <lukasz-strama@outlook.com>
2024-09-22 18:20:59 +0000

    done?

--- src/main.c
+++ src/main.c
@@ -2,16 +2,29 @@
 #define MEOW_TOK_BUFSIZE 64
 #define MEOW_TOK_DELIM " \t\r\n\a"
 
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void meow_loop(void);
 char *meow_read_line(void);
+char **meow_split_line(char *line);
+int meow_launch(char **args);
+
+int meow_cd(char **args);
+int meow_help(char **args);
+int meow_exit(char **args);
+
+int meow_execute(char **args);
 
 int main(int argc, char **argv) {
 
   // load config files if any
 
   // run command loop
+  meow_loop();
 
   // perform exit/cleanup
 
@@ -24,13 +37,18 @@ void meow_loop(void) {
   int status;
 
   do {
-    printf("> ");
+    printf("$ ");
+    line = meow_read_line();
+    args = meow_split_line(line);
+    status = meow_execute(args);
+
+    free(line);
+    free(args);
   }
   while (status);
 }
 
-char *meow_read_line(void)
-{
+char *meow_read_line(void) {
   char *line = NULL;
   size_t bufsize = 0;
 
@@ -46,14 +64,13 @@ char *meow_read_line(void)
   return line;
 }
 
-char **lsh_split_line(char *line)
-{
+char **meow_split_line(char *line) {
   int bufsize = MEOW_TOK_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token;
 
   if (!tokens) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "meow: allocation error\n");
     exit(1);
   }
 
@@ -66,7 +83,7 @@ char **lsh_split_line(char *line)
       bufsize += MEOW_TOK_BUFSIZE;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
-        fprintf(stderr, "lsh: allocation error\n");
+        fprintf(stderr, "meow: allocation error\n");
         exit(1);
       }
     }
@@ -75,4 +92,82 @@ char **lsh_split_line(char *line)
   }
   tokens[position] = NULL;
   return tokens;
+}
+
+int meow_launch(char **args) {
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    if (execvp(args[0], args) == -1) {
+      perror("meow");
+    }
+    exit(1);
+  } else if (pid < 0) {
+    perror("meow");
+  } else {
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &meow_cd,
+  &meow_help,
+  &meow_exit
+};
+
+int meow_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+int meow_cd(char **args) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "meow: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("meow");
+    }
+  }
+  return 1;
+}
+
+int meow_help(char **args) {
+  int i;
+  printf("Meow Shell - 1.0.0\n");
+
+  for (i = 0; i < meow_num_builtins(); i++) {
+    printf("- %s\n", builtin_str[i]);
+  }
+  return 1;
+}
+
+int meow_exit(char **args) {
+  return 0;
+}
+
+int meow_execute(char **args) {
+  int i;
+
+  if (args[0] == NULL) {
+    return 1;
+  }
+
+  for (i = 0; i < meow_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return meow_launch(args);
 }
