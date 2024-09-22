b483ca3bd936e24ded57433c392218443eb0b2ea
łukasz <lukasz-strama@outlook.com>
2024-09-22 17:48:30 +0000

    split line

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #define MEOW_RL_BUFSIZE 1024
+#define MEOW_TOK_BUFSIZE 64
+#define MEOW_TOK_DELIM " \t\r\n\a"
 
 #include <stdio.h>
 
@@ -42,4 +44,35 @@ char *meow_read_line(void)
   }
 
   return line;
+}
+
+char **lsh_split_line(char *line)
+{
+  int bufsize = MEOW_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(1);
+  }
+
+  token = strtok(line, MEOW_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += MEOW_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(1);
+      }
+    }
+
+    token = strtok(NULL, MEOW_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
